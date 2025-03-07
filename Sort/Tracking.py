d51064b269131328da42561a73d13c80a8a24121
hopepi <umuttozkannis@gmail.com>
2025-03-07 18:33:47 +0300

    Add CommandLine

--- Sort/Tracking.py
+++ Sort/Tracking.py
@@ -1,57 +1,60 @@
+"""
+Bu projede nesne takibi için SORT  algoritması kullanılmıştır.
+SORT, Kalman Filtresi ve IOU metriği ile çoklu nesne takibini gerçekleştiren gerçek zamanlı bir takip algoritmasıdır
+Kaynak https://github.com/abewley/sort
+Teşekkürler Alex Bewley
+"""
+
 import cv2
 import torch
 import numpy as np
-from sort import Sort  # SORT kütüphanesi
+from sort import Sort
 from ultralytics import YOLO
 
-# YOLO modelini yükle
-model = YOLO("../Models/yolov11s-face.pt")  # Yüz tespiti için uygun bir model kullan
 
-# Video kaynağını aç
-cap = cv2.VideoCapture("video.mp4")  # Video dosyası veya cap = cv2.VideoCapture(0) ile canlı kamera
+model = YOLO("../Models/yolov11s-face.pt")
 
-# SORT tracker'ı başlat
+cap = cv2.VideoCapture("video.mp4")
+
 tracker = Sort(max_age=30, min_hits=3, iou_threshold=0.3)
 
-# Pencere boyutlarını ayarla
-WINDOW_WIDTH = 800  # Genişlik
-WINDOW_HEIGHT = 600  # Yükseklik
+WINDOW_WIDTH = 800
+WINDOW_HEIGHT = 600
 
 while cap.isOpened():
     ret, frame = cap.read()
     if not ret:
-        break  # Video bittiğinde çık
+        break
 
-    # Frame'i yeniden boyutlandır (ekrana sığması için)
     frame = cv2.resize(frame, (WINDOW_WIDTH, WINDOW_HEIGHT))
 
-    # YOLO ile yüzleri tespit et
+
     results = model(frame)
 
-    detections = []  # SORT için kullanılacak tespit listesi
+    detections = []
 
     for result in results:
         for box in result.boxes:
             x1, y1, x2, y2 = box.xyxy[0].cpu().numpy()
             conf = box.conf[0].cpu().item()
 
-            if conf > 0.5:  # Güven eşiği (Yanlış tespitleri önlemek için)
-                detections.append([x1, y1, x2, y2, conf])  # [x1, y1, x2, y2, confidence]
+            if conf > 0.5:
+                detections.append([x1, y1, x2, y2, conf])
 
-    # SORT'a güncellenmiş yüz tespitlerini ver
+
     if len(detections) > 0:
         tracked_objects = tracker.update(np.array(detections))
 
         for obj in tracked_objects:
-            x1, y1, x2, y2, track_id = map(int, obj)  # Takip edilen objelerin bilgileri
-            cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)  # Yüzü çiz
+            x1, y1, x2, y2, track_id = map(int, obj)
+            cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
             cv2.putText(frame, f"ID {track_id}", (x1, y1 - 10),
                         cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 1)  # ID yazdır
 
-    # Sonucu göster
+
     cv2.imshow("Face Tracking", frame)
 
-    # Çıkmak için 'q' tuşuna bas
+
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
